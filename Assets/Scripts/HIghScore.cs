86d88cbcedae23d3669903ece28bd3f2e25c9471
sibilavayn <e.ditkovskaya96@gmail.com>
2021-09-16 15:19:38 +0300

    Init commit

---
+++ Assets/Scripts/HIghScore.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HIghScore : MonoBehaviour
+{
+
+    static public int score=1000;
+
+    void Awake()
+    {
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            score=PlayerPrefs.GetInt("HighScore");
+        }
+    }
+
+    void Update()
+    {
+        Text gt=this.GetComponent<Text>();
+        gt.text="High Score:"+score;
+
+        if (score>PlayerPrefs.GetInt("HighScore"))
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+        }
+    }
+}
